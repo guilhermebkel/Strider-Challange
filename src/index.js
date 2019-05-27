75369be51610f2d639174845e01cf10e1028b154
Guilherme <guilhermebromonschenkel@gmail.com>
2019-05-27 20:51:03 -0300

    Improved new image rendering

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const imagePath = join(__dirname , './db/', `challenge_strider.png`);
 
 // Red and white color code in RGB
 const redColorDecimalCode = ['255', '0', '0'];
-const whiteColorDecimalCode = ['255', '255', '255']
+const whiteColorDecimalCode = ['255', '255', '255'];
 
 // Decodes the Image and works
 // on the pixels got from it
@@ -47,7 +47,12 @@ const keepOnlyRedPixels = (pixels) => {
     // As a RGB color is composed of multiple values
     // we'll increment by 2.
     for(let i=0; i<(pixels.length); i+=2){
-        if(pixels[i] != redColorDecimalCode[0] && pixels[i+1] != redColorDecimalCode[1] && pixels[i+2] != redColorDecimalCode[2]){
+        if(pixels[i] == redColorDecimalCode[0] && pixels[i+1] == redColorDecimalCode[1] && pixels[i+2] == redColorDecimalCode[2]){
+            pixels[i] = redColorDecimalCode[0];
+            pixels[i+1] = redColorDecimalCode[1];
+            pixels[i+2] = redColorDecimalCode[2];
+        }
+        else{
             pixels[i] = whiteColorDecimalCode[0];
             pixels[i+1] = whiteColorDecimalCode[1];
             pixels[i+2] = whiteColorDecimalCode[2];
