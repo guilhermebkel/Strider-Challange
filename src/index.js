32157b9e102306bf12e1397de1ad9afe1b481dfa
Guilherme <guilhermebromonschenkel@gmail.com>
2019-05-28 20:54:36 -0300

    Added patternfinder function

--- src/index.js
+++ src/index.js
@@ -5,31 +5,29 @@ const PNG = require('pngjs').PNG;
 // Configures the Image Path
 const imagePath = join(__dirname , './db/', `challenge_strider.png`);
 
-// Red and white color code in RGB
+// Red color code in RGB
 const redColorDecimalCode = ['255', '0', '0'];
-const whiteColorDecimalCode = ['255', '255', '255'];
 
 // Decodes the Image and works
-// on the pixels got from it
+// on the pixels got from it.
 fs.createReadStream(imagePath)
     .pipe(new PNG())
     .on('parsed', function() {
         countRedPixels(this.data);
+        findPatternOnRedPixels(this.data);
         keepOnlyRedPixels(this.data);
 });
 
 // Counts the amount of red pixels on image
 const countRedPixels = (pixels) => {
 
-    // Saves the number of red pixels on Image
+    // Saves the amount of red pixels on image
     let redPixelsCounter = 0;
 
     // If it finds red color pixels based on
     // decimal red color code, it increments
     // the redPixelsCounter variable.
-    // As a RGB color is composed of multiple values
-    // we'll increment by 2.
-    for(let i=0; i<(pixels.length); i+=2){
+    for(let i=0; i<(pixels.length); i+=4){
         if(pixels[i] == redColorDecimalCode[0] && pixels[i+1] == redColorDecimalCode[1] && pixels[i+2] == redColorDecimalCode[2]){
             redPixelsCounter++;
         }
@@ -40,20 +38,21 @@ const countRedPixels = (pixels) => {
 // Turns all the not red pixels in white, and save a new image
 const keepOnlyRedPixels = (pixels) => {
 
+    // Black color code in RGB
+    const blackColorDecimalCode = ['0', '0', '0'];
+
     // If the pixels don't belong to red 
     // color turns it into white color.
-    // As a RGB color is composed of multiple values
-    // we'll increment by 2.
-    for(let i=0; i<(pixels.length); i+=2){
+    for(let i=0; i<(pixels.length); i+=4){
         if(pixels[i] == redColorDecimalCode[0] && pixels[i+1] == redColorDecimalCode[1] && pixels[i+2] == redColorDecimalCode[2]){
             pixels[i] = redColorDecimalCode[0];
             pixels[i+1] = redColorDecimalCode[1];
             pixels[i+2] = redColorDecimalCode[2];
         }
         else{
-            pixels[i] = whiteColorDecimalCode[0];
-            pixels[i+1] = whiteColorDecimalCode[1];
-            pixels[i+2] = whiteColorDecimalCode[2];
+            pixels[i] = blackColorDecimalCode[0];
+            pixels[i+1] = blackColorDecimalCode[1];
+            pixels[i+2] = blackColorDecimalCode[2];
         }
     }
     
@@ -64,6 +63,29 @@ const keepOnlyRedPixels = (pixels) => {
         .on('parsed', function() {
             this.data = pixels;
             this.pack().pipe(fs.createWriteStream('./src/db/challenge_strider_redpixels.png'));
-            console.log("Image with only red pixels was sucessfully saved!");
+            console.log("\nImage with only red pixels was sucessfully saved!");
     });
+}
+
+const findPatternOnRedPixels = (pixels) => {
+
+    let alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+                    'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+    let newArray = [];
+
+    for(let i=0; i<(pixels.length); i+=2){
+        if(pixels[i] == redColorDecimalCode[0] && pixels[i+1] == redColorDecimalCode[1] && pixels[i+2] == redColorDecimalCode[2]){
+            newArray.push("red");
+        }
+        else{
+            newArray.push([pixels[i], pixels[i+1], pixels[i+2]]);
+        }
+    }
+    
+    for(let i=0; i<newArray.length; i++){
+        if(newArray[i] == "red"){
+            process.stdout.write(alphabet[i%alphabet.length]);
+        }
+    }
+    
 }
